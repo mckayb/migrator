44518abaf05f8ff082e327638c212e56fac2c2db
McKay Broderick <mckay.db@gmail.com>
2017-10-21 20:03:20 +0000

    Got the combo file building. Still could use a little work. but we're getting there

--- example/src/Main.hs
+++ example/src/Main.hs
@@ -1,1 +1,6 @@
 {-# OPTIONS_GHC -F -pgmF migrator-discover #-}
+
+module Main where
+
+main :: IO ()
+main = migrator_run_migrations
--- migrator-discover/src/Migrator/Discover/Run.hs
+++ migrator-discover/src/Migrator/Discover/Run.hs
@@ -1,48 +1,73 @@
-{-# LANGUAGE FlexibleInstances, OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Migrator.Discover.Run (run) where
 
-import Prelude (String, IO, ShowS, showString, shows, fmap, foldl, (.))
+import Prelude (String, IO, ShowS, showString, shows, fmap, readFile, unlines, lines, filter, not, (/=), (&&), (.))
 import System.IO (FilePath, hPutStrLn, stderr, writeFile)
 import System.Directory (listDirectory)
 import System.FilePath.Posix (takeDirectory, dropExtension)
 import Data.String (IsString, fromString)
 import Data.List (sort)
 import Data.Semigroup ((<>))
+import Data.Text (pack, isPrefixOf)
 
 instance IsString ShowS where
   fromString = showString
 
+importsFromBody :: [String] -> [String]
+importsFromBody = filter (\x -> x /= "" && "import " `isPrefixOf` pack x && not ("module " `isPrefixOf` pack x))
+
+nonImportsFromBody :: [String] -> [String]
+nonImportsFromBody = filter (\x -> x /= "" && not ("{-" `isPrefixOf` pack x) && not ("import "  `isPrefixOf` pack x) && not ("module " `isPrefixOf` pack x))
+
+moduleFromBody :: [String] -> [String]
+moduleFromBody = filter (\x -> "module " `isPrefixOf` pack x)
+
 run :: [String] -> IO ()
 run args =
     case args of
-        -- src : _ : dst : args' -> do
         src : _ : dst : _ -> do
+            -- 1) Read the input file
+            linesInput <- fmap lines (readFile src)
+
+            -- 2) Separate the input file by imports and body
+            let moduleName = unlines (moduleFromBody linesInput)
+            let originalImports = importsFromBody linesInput
+            let originalBody = nonImportsFromBody linesInput
+
+            -- 3) Gather the migration imports and body
             migrationFiles <- listDirectory (takeDirectory src <> "/Migrations")
-            let modules = fmap dropExtension (sort migrationFiles)
-            -- hPutStrLn stderr (mkModule src modules)
-            writeFile dst (mkModule src modules)
+            let migrationModules = fmap (("Migrations." <>) . dropExtension) (sort migrationFiles)
+
+            let migrationImports = fmap (("import qualified " <>) . (<> " (main)")) migrationModules
+            let migrationBody = ["migrator_run_migrations :: IO ()", "migrator_run_migrations=do"] <> fmap (("  " <>) . (<> ".main")) migrationModules
+
+            -- 4) Combine the original and migration parts
+            let imports = unlines (originalImports <> migrationImports)
+            let body = unlines (originalBody <> migrationBody)
+
+            -- 5) Put it all together
+            writeFile dst (mkModule src moduleName imports body)
         _ -> hPutStrLn stderr "Error"
 
-mkModule :: FilePath -> [String] -> String
-mkModule src migrations =
+-- mkModule :: FilePath -> String -> [String] -> [String] -> String
+mkModule :: FilePath -> String -> String -> String -> String
+mkModule src moduleName imports body =
     ( "{-# LINE 1 " . shows src . " #-}\n"
     . showString "{-# OPTIONS_GHC -fno-warn-warnings-deprecations #-}\n"
-    . showString "module Main where\n"
-    . showString (migrationImports migrations)
+    . showString moduleName
+    . showString imports
     . showString "\n"
-    . showString "main :: IO ()\n"
-    . showString "main = do\n"
-    . showString (migrationBodies migrations)
+    . showString body
     ) "\n"
 
-migrationImports :: [String] -> String
-migrationImports = migModuleText imports
-  where imports mig = "import qualified Migrations." <> mig <> " (main)\n"
+-- migrationImports :: [String] -> String
+-- migrationImports = migModuleText imports
+  -- where imports mig = "import qualified Migrations." <> mig <> " (main)\n"
 
-migrationBodies :: [String] -> String
-migrationBodies = migModuleText body
-  where body mig = "  Migrations." <> mig <> ".main\n"
+-- migrationBodies :: [String] -> String
+-- migrationBodies = migModuleText body
+  -- where body mig = "  Migrations." <> mig <> ".main\n"
 
-migModuleText :: (String -> String) -> [String] -> String
-migModuleText f migs = foldl (<>) "" (fmap f migs)
+-- migModuleText :: (String -> String) -> [String] -> String
+-- migModuleText f migs = foldl (<>) "" (fmap f migs)
