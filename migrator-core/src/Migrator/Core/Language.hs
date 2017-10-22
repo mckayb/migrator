98ad2c4defeb6f27cba200a55cc9c14a0b3a9059
McKay Broderick <mckay.db@gmail.com>
2017-10-22 03:17:40 +0000

    Got started on the languages.

--- migrator-core/src/Migrator/Core/Language.hs
+++ migrator-core/src/Migrator/Core/Language.hs
@@ -5,9 +5,32 @@ module Migrator.Core.Language where
 test :: IO ()
 test = putStrLn "hello world"
 
-data Object = Table | Column
+data CreateObject a =
+  CreateDatabase String (Maybe [Options a])
+  | CreateTable String (Maybe [Options a])
+  | CreateColumn String String (Maybe [Options a])
 
-data Expr a = 
-  Create a | Alter a | Drop a
+data DropObject =
+  DropDatabase String
+  | DropTable String
+  | DropColumn String
 
-type Migration = Expr Object
+data Expr a b = 
+  Create a b | Alter a b | Drop a
+
+data Options a = Default a | Nullable
+
+type Add = Create
+type Modify = Alter
+
+
+-- mig = Alter (Table "blah") Add (Column "col_name" "col_type" Just [Default "blue", Nullable])
+-- mig = Alter (Table "blah") Create (Column "col_name" "col_type" Just [Default "blue", Nullable])
+-- mig = Alter (Table "blah") Modify (Column "col_name" "col_type" Just [Default "blue", Nullable])
+-- mig = Alter (Table "blah") Drop (Column "col_name")
+
+-- mig = Create (Table "blah" Nothing)
+-- mig = Create (Database "blah" Nothing)
+
+-- mig = Drop (Table "blah")
+-- mig = Drop (Database "blah")
